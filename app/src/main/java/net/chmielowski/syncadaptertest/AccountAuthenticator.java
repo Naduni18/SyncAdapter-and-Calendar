76508fb4f477a3d2dfa0e00aa1acf1a0f57a6ff8
Piotrek Chmielowski <piotrek.chmielowski@interia.pl>
2018-05-04 12:27:50 +0200

    LoginActivity removed

--- app/src/main/java/net/chmielowski/syncadaptertest/AccountAuthenticator.java
+++ app/src/main/java/net/chmielowski/syncadaptertest/AccountAuthenticator.java
@@ -4,18 +4,14 @@ package net.chmielowski.syncadaptertest;
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
-import android.accounts.AccountManager;
 import android.accounts.NetworkErrorException;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class AccountAuthenticator extends AbstractAccountAuthenticator {
-    private final Context context;
 
     public AccountAuthenticator(final Context context) {
         super(context);
-        this.context = context;
     }
 
     @Override
@@ -29,9 +25,7 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator {
             final AccountAuthenticatorResponse response, final String accountType,
             final String authTokenType, final String[] requiredFeatures,
             final Bundle options) throws NetworkErrorException {
-        final Bundle bundle = new Bundle();
-        bundle.putParcelable(AccountManager.KEY_INTENT, new Intent(context, LoginActivity.class));
-        return bundle;
+        throw new UnsupportedOperationException();
     }
 
     @Override
